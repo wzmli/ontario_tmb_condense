cdc9c34efb6cec4bf5aad75c1d02ec6a519fbd30
wzmli <wzmichael.li@gmail.com>
2022-05-19 15:32:51 -0400

    adding both calibration data and input data

--- data.R
+++ data.R
@@ -1,12 +1,14 @@
 library(tidyverse)
+library(jsonlite)
+
 
 ## Reading in Ontario public data
 ontario_dat <- read_csv("https://data.ontario.ca/datastore/dump/ed270bb8-340b-41f9-a7c6-e8ef587e6d11?bom=True")
 
 print(ontario_dat)
 
-## Cleaning and picking the time series of interest
-clean_dat <- (ontario_dat
+## Cleaning and picking the time series of interest for calibration
+calibration_dat <- (ontario_dat
 	%>% transmute(date = as.Date(`Reported Date`)
 		, report_inc = diff(c(`Total Cases`,0))
 		, hosp_preval = `Number of patients hospitalized with COVID-19`
@@ -15,6 +17,26 @@ clean_dat <- (ontario_dat
 	%>% pivot_longer(names_to = "var", -"date")
 )
 
-print(clean_dat)
+print(calibration_dat)
 
+## Input data (vaccination)
 
+prov <- "ON"
+
+url <- paste0('https://api.covid19tracker.ca/reports/province/'
+	, prov
+	, '?fill_dates=true'
+)
+
+vaccine_database <- fromJSON(url)
+vaccine_dat <- (upd$data
+	%>% transmute(province = prov
+			, date
+			, total_vaccinations
+			, total_vaccinated
+			, total_boosters_1
+			, total_boosters_2
+	)
+)
+
+print(vaccine_dat)
