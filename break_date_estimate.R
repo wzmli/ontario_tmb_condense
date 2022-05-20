cc620f9d7b5623ab7c759609fa6fd46488a41102
wzmli <wzmichael.li@gmail.com>
2022-05-20 02:24:12 -0400

    Separating input data from observed data script and add break point estimate method

---
+++ break_date_estimate.R
@@ -0,0 +1,41 @@
+library(segmented)
+library(tidyverse)
+library(lubridate)
+library(zoo)
+library(data.table)
+source("observed_data.R")
+
+
+## filter on observed report time series
+## smooth it with log 7 day moving average
+report_dat <- (calibration_dat
+	%>% filter(var == "report_inc")
+	%>% mutate(lmavg = log(frollmean(value,n=7,align = "right")))
+	%>% filter(!is.infinite(lmavg))
+)
+
+## check
+gg <- ggplot(report_dat,aes(x=date,y=lmavg)) + geom_line() 
+gg
+
+## fit a loglinear regression
+fit <- lm(lmavg ~ date,data=report_dat)
+
+## refit with piecewise break dates. We can pick as many break points as we want
+## here, we are arbitrary picking 14 (we had roughly 6 waves)
+
+n.breaks <- 14
+refit <- segmented(fit, seg.Z = ~date, npsi=n.breaks)
+
+## extract breakdates
+break_date <- as.Date(c(round(refit$indexU$date)))
+
+gg + geom_vline(xintercept=break_date)
+
+reporting_lag <- 8
+beta_break_dates <- data.frame(Date = break_date - reporting_lag
+	, Symbol = "beta0"
+	, Value = NA
+)
+
+print(beta_break_dates)
--- observed_data.R
+++ observed_data.R
@@ -1,6 +1,4 @@
 library(tidyverse)
-library(jsonlite)
-
 
 ## Reading in Ontario public data
 ontario_dat <- read_csv("https://data.ontario.ca/datastore/dump/ed270bb8-340b-41f9-a7c6-e8ef587e6d11?bom=True")
@@ -10,7 +8,7 @@ print(ontario_dat)
 ## Cleaning and picking the time series of interest for calibration
 calibration_dat <- (ontario_dat
 	%>% transmute(date = as.Date(`Reported Date`)
-		, report_inc = diff(c(`Total Cases`,0))
+		, report_inc = diff(c(0,`Total Cases`))
 		, hosp_preval = `Number of patients hospitalized with COVID-19`
 		, icu_preval = `Number of patients in ICU due to COVID-19`
 	)
@@ -18,23 +16,3 @@ calibration_dat <- (ontario_dat
 )
 
 print(calibration_dat)
-
-## Input data (vaccination)
-
-url <- paste0('https://api.covid19tracker.ca/reports/province/'
-	, prov
-	, '?fill_dates=true'
-)
-
-vaccine_database <- fromJSON(url)
-vaccine_dat <- (vaccine_database$data
-	%>% transmute(province = prov
-		, date
-		, total_vaccinations
-		, total_vaccinated
-		, total_boosters_1
-		, total_boosters_2
-	)
-)
-
-print(vaccine_dat)
