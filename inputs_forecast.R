394a749cc1ef1253520459298184d5723fc02f11
Irena Papst <ip98@cornell.edu>
2022-05-24 11:52:50 -0400

    first pass at inputs for forecasting

---
+++ inputs_forecast.R
@@ -0,0 +1,10 @@
+## number of days to forecast past the calibration end date
+n_days_forecast <- 30
+
+## new rows for params_timevar in the forecast period
+params_timevar_forecast <- data.frame(
+  Date = model_calibrated$end_date + 1, ## start on the date after the calibration end date
+  Symbol = "beta0",
+  Value = 1.2, ## increase last beta0 by 20%
+  Type = "rel_prev"
+)
