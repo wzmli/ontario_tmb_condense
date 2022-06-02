7d4051a2079ee39f647c79b1caed88b946ca6f39
Irena Papst <ip98@cornell.edu>
2022-06-02 13:42:39 -0400

    generate `params_timevar_vaccine` for the two-dose model

--- inputs_vaccine.R
+++ inputs_vaccine.R
@@ -1,8 +1,16 @@
 library(tidyverse)
 library(jsonlite)
 
-prov <- "ON"
+## plot settings
+theme_set(theme_bw())
+theme_update(
+  axis.title.y = element_blank()
+  , strip.background = element_rect(fill = "white")
+)
+fig.width <- 6 ## inches
 
+## data inputs
+prov <- "ON"
 url <- paste0('https://api.covid19tracker.ca/reports/province/'
               , prov
               , '?fill_dates=true'
@@ -49,14 +57,19 @@ vaccine_tidy <- (vaccine_raw
   )
   %>% rename_with(~ str_replace(.x, "prev", "inc"),
                   ends_with("prev"))
-  ## replace NAs with 0 for all numeric columns
+  ## replace negative incidence or NA
+  ## with 0 for all numeric columns
   %>% mutate(across(where(is.numeric),
-                    ~ ifelse(is.na(.x), 0, .x)))
+                    ~ ifelse((.x < 0 | is.na(.x)), 0, .x)))
 )
 
 ## plot for diagnostics
 vaccine_plot <- (
   vaccine_tidy
+  ## drop days where no vaccine was administered
+  %>% mutate(total = rowSums(across(ends_with("inc"))))
+  %>% filter(total != 0)
+  %>% select(-total)
   %>% pivot_longer(-date)
 )
 p1 <- (ggplot(vaccine_plot,
@@ -76,16 +89,67 @@ p1 <- (ggplot(vaccine_plot,
                strip.position = "top")
   + guides(colour = "none")
   + labs(title = "Daily new doses")
-  + theme(
-    axis.title.y = element_blank()
-  )
 )
 ggsave(
   file.path("figs","inputs_vaccine.png"),
   p1,
+  width = fig.width,
+  height = 1.3*fig.width
 )
 
 ## fit/forecast (if need be)
 
 ## final output (processed ts or params_timevar df)
 
+## for the two-dose model
+params_timevar_vaccine <- (
+  vaccine_tidy
+  ## keep only first and second doses
+  %>% select(
+    date
+    , matches("^dose(1|2)")
+  )
+  ## sum total daily doses
+  %>% mutate(doseall_inc = rowSums(across(where(is.numeric))))
+  ## drop days where no vaccines were administered
+  %>% filter(doseall_inc != 0)
+  ## calculate parameters for two-dose model
+  %>% transmute(
+    date
+    , vax_doses_per_day = doseall_inc
+    , vax_prop_first_dose = dose1_inc/vax_doses_per_day
+  )
+  ## format as params_timevar
+  %>% pivot_longer(
+    -date,
+    names_to = "Symbol",
+    values_to = "Value"
+  )
+  %>% rename(Date = date)
+  %>% mutate(Type = "abs")
+  %>% as.data.frame()
+)
+
+# plot to check
+p2 <- (ggplot(
+  params_timevar_vaccine,
+  aes(x = Date, y = Value, colour = Symbol)
+  )
+  + geom_point(alpha = 0.3)
+  + facet_wrap(
+    ~ Symbol,
+    ncol = 1,
+    scales = "free_y",
+    strip.position = "top"
+  )
+  + guides(colour = "none")
+  + labs(title = "Time-varying parameters input for vaccination",
+         x = "date")
+)
+
+ggsave(
+  file.path("figs","inputs_vaccine_params_timevar.png"),
+  p2,
+  width = fig.width,
+  height = fig.width
+)
