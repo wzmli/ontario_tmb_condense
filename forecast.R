fe08292633aad71069f851a2ca89ce8e8a3aa6c5
Irena Papst <ip98@cornell.edu>
2022-06-02 09:43:00 -0400

    bugfix for last commit

--- forecast.R
+++ forecast.R
@@ -9,6 +9,6 @@ forecast_intervals = (model_to_forecast
   + facet_wrap(~var)
   + geom_line(aes(Date, value))
   + geom_ribbon(aes(Date, ymax = upr, ymin = lwr), alpha = 0.5)
-  + geom_point(aes(date, value), data = model_calibrated$observed$data, colour = 'red', size = 1.5, alpha = 0.2)
+  + geom_point(aes(date, value), data = model_calibrated$observed$data %>% rename(name = var), colour = 'red', size = 1.5, alpha = 0.2)
   + facet_grid(rows = vars(name))
 )
