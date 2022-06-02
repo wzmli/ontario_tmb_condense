a80a101f5fc3998307cdfcefade6156220600579
Irena Papst <ip98@cornell.edu>
2022-06-02 09:30:45 -0400

    adapting forecast graph to show multiple variables

--- forecast.R
+++ forecast.R
@@ -9,5 +9,6 @@ forecast_intervals = (model_to_forecast
   + facet_wrap(~var)
   + geom_line(aes(Date, value))
   + geom_ribbon(aes(Date, ymax = upr, ymin = lwr), alpha = 0.5)
-  + geom_line(aes(date, value), data = model_calibrated$observed$data, colour = 'red')
+  + geom_point(aes(date, value), data = model_calibrated$observed$data, colour = 'red', size = 1.5, alpha = 0.2)
+  + facet_grid(rows = vars(name))
 )
