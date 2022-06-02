d15d8a0ca177aca43eade1369d5b567da05b52ab
Irena Papst <ip98@cornell.edu>
2022-06-02 14:03:06 -0400

    calibration and forecast output plots

--- calibration.R
+++ calibration.R
@@ -1,17 +1,25 @@
 model_calibrated = calibrate_flexmodel(model_uncalibrated, optimizer = 'nlminb')
 convergence_info(model_calibrated)
 
-(model_calibrated
+p1 <- (model_calibrated
   %>% fitted
   %>% ggplot
   + facet_wrap(~var)
-  + geom_line(aes(date, value))
+  + geom_point(aes(date, value),
+               alpha = 0.3, size = 1.5)
   + geom_line(aes(date, value_fitted), colour = 'red')
   + geom_vline(
     aes(xintercept = Date),
     linetype = 'dashed',
     data = filter(params_timevar, Symbol == "beta0")
   )
+)
+fig.width <- 6
+ggsave(
+  file.path("figs", "calibration.png"),
+  p1,
+  width = fig.width,
+  height = 0.4*fig.width
 )
 
 model_calibrated$opt_par
--- forecast.R
+++ forecast.R
@@ -5,10 +5,24 @@ model_to_forecast = (model_calibrated
 forecast_intervals = (model_to_forecast
   %>% simulate_ensemble(PDify = TRUE)
 )
-(ggplot(forecast_intervals)
+p1 <- (ggplot(forecast_intervals)
   + facet_wrap(~var)
-  + geom_line(aes(Date, value))
-  + geom_ribbon(aes(Date, ymax = upr, ymin = lwr), alpha = 0.5)
-  + geom_point(aes(date, value), data = model_calibrated$observed$data %>% rename(name = var), colour = 'red', size = 1.5, alpha = 0.2)
-  + facet_grid(rows = vars(name))
+  + geom_ribbon(aes(Date, ymax = upr, ymin = lwr),
+                alpha = 0.5)
+  + geom_point(aes(date, value), data = model_calibrated$observed$data %>% rename(name = var),
+               size = 1.5, alpha = 0.2)
+  + geom_line(aes(Date, value), colour = 'red')
+  + facet_wrap(
+    ~ name
+    , ncol = 1
+    , scales = "free_y"
+    , strip.position = "top"
+    )
+)
+fig.width = 6
+ggsave(
+  file.path("figs", "forecast.png"),
+  p1,
+  width = fig.width,
+  height = 1.3*fig.width
 )
