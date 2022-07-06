92f93fd9ee2c4dd57d2534ab503e1f10006be93a
Irena Papst <ip98@cornell.edu>
2022-07-06 16:47:08 -0400

    invert obs scaling in forecast

--- forecast.R
+++ forecast.R
@@ -14,6 +14,27 @@ forecast_ensemble = (model_to_forecast
   %>% filter(var %in% unique(model_calibrated$observed$data$var))
 )
 
+## invert scaling in forecast_ensemble if present
+load(
+  file.path("pipeline_environments",
+            paste0("calibration_env_",
+                   calib_date,
+                   ".Rdata"))
+)
+if("scale_factor" %in% names(calibration_dat)){
+  forecast_ensemble <- (forecast_ensemble
+     %>% rename(date = Date)
+     %>% left_join(calibration_dat %>% select(-value), by = c("date", "var"))
+     %>% replace_na(list(scale_factor = 1))
+     %>% mutate(
+       value = value/scale_factor,
+       lwr = lwr/scale_factor,
+       upr = upr/scale_factor
+    )
+   %>% rename(Date = date)
+  )
+}
+
 # ---------------------------
 # Script output
 # ---------------------------
