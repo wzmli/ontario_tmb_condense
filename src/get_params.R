46d85d16337ecdfa58f9803fe886f8aa8d80f319
Irena Papst <papstirena@gmail.com>
2022-08-03 15:22:29 -0400

    a little error handling when loading parameters

--- src/get_params.R
+++ src/get_params.R
@@ -22,21 +22,29 @@ variant_map <- (
   %>% as.data.frame()
 )
 
-## load invading variant properties, including label, corresponding start date, end date, and vaccine efficacies against each variant
-invader_properties <- (bind_rows(
-  ## default variant params
-  suppressMessages(read_sheet(
+## default variant params
+invader_default <- suppressMessages(read_sheet(
     params_url,
     sheet = "variant_default",
     col_types = 'c' ## in order to parse fractional values later
   ))
-,
-  ## region-specific variant params
-  suppressMessages(read_sheet(
+
+if(!(paste0("variant_", region) %in% sheet_names(params_url))){
+  stop(paste0("please prepare variant_", region, " sheet in parameter spreadsheet"))
+}
+
+## region-specific variant params
+invader_pt <- suppressMessages(read_sheet(
     params_url,
     sheet = paste0("variant_", region),
     col_types = 'c' ## in order to parse fractional values later
-  )))
+  ))
+
+## load invading variant properties, including label, corresponding start date, end date, and vaccine efficacies against each variant
+invader_properties <- (bind_rows(
+    invader_default
+  , invader_pt
+  )
   %>% select(label, symbol, value)
   %>% pivot_wider(id_cols = "label",
                   names_from = "symbol")
